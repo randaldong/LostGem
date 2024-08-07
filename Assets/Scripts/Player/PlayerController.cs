6b39bd03bcfb2010654e2e47fc9886eff7d63e8c
Randal Dong <ryan.dr@qq.com>
2024-08-07 14:20:53 -0700

    movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float moveSpeed = 10;
+	
+	private PlayerInputControl inputControl;
+	private Vector2 moveDirection;
+	private Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Awake()
+	{
+		inputControl = new PlayerInputControl();
+		moveDirection = new Vector2(0, 0);
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnEnable()
+	{
+		inputControl.Enable();
+	}
+
+	private void OnDisable()
+	{
+		inputControl.Disable();
+	}
+
+	private void Update()
+	{
+		moveDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+	}
+
+	// Update physics
+	private void FixedUpdate()
+	{
+		Move();
+	}
+
+	private void Move()
+	{
+		// x for keyboard control, y for falling
+		rb.velocity = new Vector2(moveSpeed * moveDirection.x, rb.velocity.y);
+
+		// flip when input direction is the opposite of the current direction
+		float faceDir = transform.localScale.x;
+
+		if (moveDirection.x * faceDir < 0)
+			faceDir = -faceDir;
+
+		transform.localScale = new Vector3(faceDir, 1, 1);
+
+	}
 }
